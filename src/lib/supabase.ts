a7f36415cbab5b133aea850dc802faad3b681f06
Juurai <126709022+JhairEsp@users.noreply.github.com>
2026-05-10 00:01:45 -0500

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Prioridad: 1. Variables de entorno (Vercel) | 2. Tus credenciales directas (Respaldo)
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://ihodhcdxxzkmhqdbmanm.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlob2RoY2R4eHprbWhxZGJtYW5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5OTMwMjAsImV4cCI6MjA5MjU2OTAyMH0.oW_EGXkGcNYDYc269FhBsSCMFZd8SooGWoLyrNK1Cg8';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://ouziqhknofdyvkbvumex.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91emlxaGtub2ZkeXZrYnZ1bWV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzODQ1MTcsImV4cCI6MjA5Mzk2MDUxN30.iE0w8BMH5VgfK7btISKv7RPA5w2-alkxDrUBGCQMbQc';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
